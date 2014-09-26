0ac1b8672498b11dd8c0f67459f8b2725e6420b2
FrancisGutierrez <francisgutierrezespada@gmail.com>
2014-09-26 17:39:04 +0200

    Arreglo matrícula coche 4

--- ComparadorCoches/src/Comparador.java
+++ ComparadorCoches/src/Comparador.java
@@ -7,7 +7,7 @@ public class Comparador {
 		Coche c1 = new Coche("4590-GNN", 1990, 90);
 		Coche c2 = new Coche("3218-UDE", 3000, 200);
 		Coche c3 = new Coche("8675-OIF", 900, 50);
-		Coche c4 = new Coche("enter", 8000, 950);
+		Coche c4 = new Coche("6952-HGF", 8000, 950);
 						
 		ArrayList<Coche> list = new ArrayList<Coche>();
 		

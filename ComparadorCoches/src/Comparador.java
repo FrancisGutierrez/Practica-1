fdd0bca32615acd4513d64c934e6bcb172f47342
FrancisGutierrez <francisgutierrezespada@gmail.com>
2014-09-26 17:09:06 +0200

    Primera subida

---
+++ ComparadorCoches/src/Comparador.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+
+public class Comparador {
+	public static void main(String[]args){
+		Coche c1 = new Coche("4590-GNN", 1990, 90);
+		Coche c2 = new Coche("3218-UDE", 3000, 200);
+		Coche c3 = new Coche("8675-OIF", 900, 50);
+		
+		ArrayList<Coche> list = new ArrayList<Coche>();
+		
+		list.add(c1);
+		list.add(c2);
+		list.add(c3);
+	}
+}

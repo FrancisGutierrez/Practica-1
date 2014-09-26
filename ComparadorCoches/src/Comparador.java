05998c26729248a4b30cf19a53cc84087c090be1
FrancisGutierrez <francisgutierrezespada@gmail.com>
2014-09-26 17:15:20 +0200

    4to coche added

--- ComparadorCoches/src/Comparador.java
+++ ComparadorCoches/src/Comparador.java
@@ -6,11 +6,13 @@ public class Comparador {
 		Coche c1 = new Coche("4590-GNN", 1990, 90);
 		Coche c2 = new Coche("3218-UDE", 3000, 200);
 		Coche c3 = new Coche("8675-OIF", 900, 50);
+		Coche c4 = new Coche("6546-GSR", 8000, 950);
 		
 		ArrayList<Coche> list = new ArrayList<Coche>();
 		
 		list.add(c1);
 		list.add(c2);
 		list.add(c3);
+		list.add(c4);
 	}
 }
